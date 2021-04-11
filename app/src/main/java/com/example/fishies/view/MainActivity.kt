192b0d9c086ee629d84d47753726926853e88bfa
soziele <sonia.ziele@gmail.com>
2021-04-11 21:44:32 +0200

    Fragments draft added

---
+++ app/src/main/java/com/example/fishies/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fishies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fishies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
